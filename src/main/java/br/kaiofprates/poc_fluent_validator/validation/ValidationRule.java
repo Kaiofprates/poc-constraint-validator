4b1ca0a79b44add9f3c72bd67c064c835594e7a6
leandro-yamaniha <leandrosy@gmail.com>
2025-05-10 08:11:45 -0300

    refact: chave pix patterns and validator

--- src/main/java/br/kaiofprates/poc_fluent_validator/validation/ValidationRule.java
+++ src/main/java/br/kaiofprates/poc_fluent_validator/validation/ValidationRule.java
@@ -10,13 +10,11 @@ import java.util.function.Predicate;
 public class ValidationRule<T> {
     private final Predicate<T> predicate;
     private final ValidationMessage validationMessage;
-    private final String field;
 
     public static <T> ValidationRule<T> of(Predicate<T> predicate, ValidationMessage validationMessage) {
         return ValidationRule.<T>builder()
                 .predicate(predicate)
                 .validationMessage(validationMessage)
-                .field(validationMessage.getField())
                 .build();
     }
 } 
